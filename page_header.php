fd65db0365431696aed38ff3fc4aa4d3ff73935e
Isaac Spackman <isaacjspackman@gmail.com>
2022-06-30 21:13:23 -0700

    try using .php files to share html from multiple pages

---
+++ page_header.php
@@ -0,0 +1,392 @@
+<?php
+    <nav>
+        <ul class="gnav">
+            <li>
+                <a href="overview.html">Overview  </a>
+            </li>
+            <li>
+                <a href="climate&energy.html">Climate & Energy </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Climate Modeling </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Global Model</a></li>
+                            <li><a href="#">Local Model</a></li>
+                            <li><a href="#">Weather?</a></li>
+                        </ul>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Carbon Accounting </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Control Volumes</a></li>
+                            <li><a href="#">Deschutes National Forest</a></li>
+                            <li><a href="#">EV Batteries</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Climate Policy </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">History</a></li>
+                            <li><a href="#">Limits to Growth</a></li>
+                            <li><a href="#">Carbon Taxes</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html"> Energy Research </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Power Generation</a></li>
+                            <li><a href="#">Power Storage</a></li>
+                            <li><a href="#">Modeling Plants</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#">Instrumentation  </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Spectroscopy </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">UV/Visible Spectroscopy</a></li>
+                            <li><a href="#">IR Spectroscopy</a></li>
+                            <li><a href="#">Raman Spectroscopy</a></li>
+                            <li><a href="#">NMR Spectroscopy</a></li>
+                        </ul>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Microscopy </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Visible Microscopy</a></li>
+                            <li><a href="#">Confocal Microscopy</a></li>
+                        </ul>
+
+                        <h3>
+                            <a href="climate&energy.html">Separations </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Gas Chromatography</a></li>
+                            <li><a href="#">Liquid Chromatography</a></li>
+                            <li><a href="#">Field Flow Fractionation</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Mechanical </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Strain Gauges</a></li>
+                            <li><a href="#">Temperature Control</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Electrical </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Oscilloscope</a></li>
+                            <li><a href="#">Potentiostat</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Miscellaneous </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Nodal Geophone</a></li>
+                            <li><a href="#">Turbidimeter</a></li>
+                            <li><a href="#">Radio Telescope</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#">Lab Equipment  </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Autosampler </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Mechanics</a></li>
+                            <li><a href="#">Hardware + Software</a></li>
+                            <li><a href="#">Controls</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Pipette Gantry </a>
+                        </h3>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Autotitrator </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Mechanics</a></li>
+                            <li><a href="#">Hardware + Software</a></li>
+                            <li><a href="#">Controls</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Product Collection</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Mechanics</a></li>
+                            <li><a href="#">Hardware + Software</a></li>
+                            <li><a href="#">Controls</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Reactor </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Mechanics</a></li>
+                            <li><a href="#">Hardware + Software</a></li>
+                            <li><a href="#">Controls</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#">Computational Studies  </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Miscibility and Solvation </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Theory</a></li>
+                            <li><a href="#">Methods</a></li>
+                            <li><a href="#">Results</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">GUI for Open Source Computational Chemistry </a>
+                        </h3>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Reduction Potential and Reversibility </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Theory</a></li>
+                            <li><a href="#">Methods</a></li>
+                            <li><a href="#">Results</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Blind Signal Separation and PERGA </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Theory</a></li>
+                            <li><a href="#">Methods</a></li>
+                            <li><a href="#">Results</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Molecular Dynamics and Docking </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Theory</a></li>
+                            <li><a href="#">Methods</a></li>
+                            <li><a href="#">Results</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#">Data Science  </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                        <a href="climate&energy.html">Reinforcement Learning </a>
+                        </h3>
+
+                        <h3>
+                            <a href="climate&energy.html">Universal Functions Originators </a>
+                        </h3>
+
+                        <h3>
+                            <a href="climate&energy.html">Classification </a>
+                        </h3>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Bootstrapping </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Sample Sizes</a></li>
+                            <li><a href="#">Curve Fit Parameters</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Data Sampling </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Selecting a Train and Test Samples</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        
+                        <h3>
+                            <a href="climate&energy.html">Statistical Design of Experiments </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Full Factorial Design</a></li>
+                            <li><a href="#">Fractional Factorial Designs and Aliasing</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Visualization and Dashboards </a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Flask</a></li>
+                            <li><a href="#">Tableau</a></li>
+                        </ul>
+                    
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#">Pyscript  </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">???</a>
+                        </h3>
+
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#">Miscellaneous  </a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Fermentation</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Beer</a></li>
+                            <li><a href="#">Wine</a></li>
+                            <li><a href="#">Kombucha</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Biomaterials</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Mycellium</a></li>
+                            <li><a href="#">SCOBY Leather</a></li>
+                        </ul>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Compliant Mechanisms</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Bi-stable Joints</a></li>
+                            <li><a href="#">Universal Joints</a></li>
+                            <li><a href="#">Force Multipliers</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Legged Robots</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Straight Line Mechanisms</a></li>
+                            <li><a href="#">Klann Mechanism</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Microfluidics</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">3D Printing</a></li>
+                            <li><a href="#">Applications</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Aquaponics</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Appropriate Crops</a></li>
+                            <li><a href="#">Controls System</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Multi-Threading</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Hardware + Software</a></li>
+                            <li><a href="#">Applications</a></li>
+                        </ul>
+                        <h3>
+                            <a href="climate&energy.html">Homebrew Styles Machine Learning Classifier</a>
+                        </h3>
+                    
+                        <h3>
+                            <a href="climate&energy.html">Glassworking</a>
+                        </h3>
+                        <ul>
+                            <li><a href="#">Scientific Glassware</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="about.html">About</a>
+                <div class="drop-down">
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Contact</a>
+                        </h3>
+                    </div>
+                
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Paper of the Week</a>
+                        </h3>
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Resources</a>
+                        </h3>
+                    
+                    </div>
+
+                    <div class="nav-column">
+                        <h3>
+                            <a href="climate&energy.html">Future Directions</a>
+                        </h3>
+
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </nav>
+?>
